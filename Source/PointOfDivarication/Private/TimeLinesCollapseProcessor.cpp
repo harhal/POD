e407283524754258c3e700cd7023441ebe3fec8d
vadzim.kharkhaliou <vadzim.kharkhaliou@vizor-games.com>
2021-06-27 12:29:50 +0300

    Added timelines collapse

---
+++ Source/PointOfDivarication/Private/TimeLinesCollapseProcessor.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TimeLinesCollapseProcessor.h"
+
+#include "TimeLineInterface.h"
+#include "Kismet/GameplayStatics.h"
+
+void UTimeLinesCollapseProcessor::CollapseTimeLine(ETimeLine MainTimeLine)
+{
+	TArray<AActor*> TimeLinesActors;
+	UGameplayStatics::GetAllActorsWithInterface(this, UTimeLineInterface::StaticClass(), TimeLinesActors);
+
+	for (AActor* Actor : TimeLinesActors)
+	{
+		const ETimeLine ActorTimeLine = ITimeLineInterface::Execute_GetTimeLine(Actor);
+
+		if (ActorTimeLine == ETimeLine::Synchronous)
+		{
+			continue;
+		}
+		
+		if (ActorTimeLine != MainTimeLine)
+		{
+			Actor->Destroy();
+		}
+		else
+		{
+			ITimeLineInterface::Execute_SetTimeLine(Actor, ETimeLine::Synchronous);
+		}		
+	}
+}
---
+++ Source/PointOfDivarication/Public/TimeLinesCollapseProcessor.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "TimeLine.h"
+#include "Components/ActorComponent.h"
+#include "TimeLinesCollapseProcessor.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class POINTOFDIVARICATION_API UTimeLinesCollapseProcessor : public UActorComponent
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable)
+	void CollapseTimeLine(ETimeLine MainTimeLine);
+};
